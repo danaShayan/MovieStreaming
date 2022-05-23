09685ab807f1ead7090bbdca5e0840fd65cac602
danaShayan <moradi.dana100@gmail.com>
2022-05-23 18:27:57 +0430

    Add video player

--- app/src/main/java/com/example/moviestreaming/Activities/VideoPlayActivity.java
+++ app/src/main/java/com/example/moviestreaming/Activities/VideoPlayActivity.java
@@ -3,15 +3,18 @@ package com.example.moviestreaming.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.moviestreaming.R;
+import com.halilibo.bettervideoplayer.BetterVideoPlayer;
 
 public class VideoPlayActivity extends AppCompatActivity {
 
 
     Bundle bundle;
-
+    BetterVideoPlayer player;
 
 
     @Override
@@ -20,6 +23,12 @@ public class VideoPlayActivity extends AppCompatActivity {
         setContentView(R.layout.activity_video_play);
 
         bundle = getIntent().getExtras();
+        player = findViewById(R.id.videoPlayer);
+
+        player.setSource(Uri.parse(ShowDetailMovieActivity.LINK_MOVIE));
+        player.setAutoPlay(true);
+        player.start();
+
 
 
     }

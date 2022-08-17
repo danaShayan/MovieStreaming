b8186a9c98b37d04d448b491307695d14ba45495
danaShayan <moradi.dana100@gmail.com>
2022-08-17 18:26:14 +0430

    Finish Episode 70

--- app/src/main/java/com/example/moviestreaming/Activities/VerifyPhoneActivity.java
+++ app/src/main/java/com/example/moviestreaming/Activities/VerifyPhoneActivity.java
@@ -1,6 +1,7 @@
 package com.example.moviestreaming.Activities;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -11,8 +12,16 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.chaos.view.PinView;
 import com.example.moviestreaming.R;
+
+import org.json.JSONObject;
 
 public class VerifyPhoneActivity extends AppCompatActivity {
 
@@ -20,15 +29,16 @@ public class VerifyPhoneActivity extends AppCompatActivity {
     AppCompatButton send, check, sendAgain;
     RelativeLayout verify_parent;
     LinearLayout check_code_layout;
-
     EditText phone;
-
-
     PinView pinView;
 
 
     static String Phone = "";
-    String url = "https://api.kavenegar.com/v1/6A335174704E68456C665A424C50566E41705874647873516970333559723757644649492B4B386B342F673D/verify/lookup.json?receptor=09922376098&token=1234&template=movieStreamingApp";
+    String url = "https://api.kavenegar.com/v1/6A335174704E68456C665A424C50566E41705874647873516970333559723757644649492B4B386B342F673D/verify/lookup.json?";
+    private int random_verify_code;
+
+    RequestQueue requestQueue;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +53,7 @@ public class VerifyPhoneActivity extends AppCompatActivity {
 
 
     private void init() {
+        requestQueue = Volley.newRequestQueue(this);
 
         send = findViewById(R.id.send_code);
         check = findViewById(R.id.check_code);
@@ -67,6 +78,7 @@ public class VerifyPhoneActivity extends AppCompatActivity {
                     verify_parent.setVisibility(View.GONE);
                     check_code_layout.setVisibility(View.VISIBLE);
                 }
+                sendCodeVerify();
             }
         });
         sendAgain.setOnClickListener(new View.OnClickListener() {
@@ -84,14 +96,35 @@ public class VerifyPhoneActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 int code = Integer.parseInt(pinView.getText().toString());
-                Toast.makeText(VerifyPhoneActivity.this, code+"", Toast.LENGTH_SHORT).show();
                 if (code == 1111) {
-                    pinView.setLineColor(R.color.green);
+                    pinView.setLineColor(Color.GREEN);
                 } else {
-                    pinView.setLineColor(R.color.red);
+                    pinView.setLineColor(Color.RED);
                 }
+
 
             }
         });
+    }
+
+    private void sendCodeVerify() {
+        random_verify_code = (int) (Math.random() * 10000);
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url + "receptor=" + Phone + "8&token=" + random_verify_code + "&template=movieStreamingApp", null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+
+        requestQueue.add(request);
     }
 }
